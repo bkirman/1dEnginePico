44c3de3d1a531d96121c63f22ebfde1e93d96a6f
Ben Kirman <ben@kirman.org>
2025-01-07 12:57:26 +0000

    Got the qwstpad working with i2c

---
+++ qwstpad_proof_of_concept.py
@@ -0,0 +1,31 @@
+from machine import Pin
+from utime import sleep
+from machine import I2C
+from qwstpad import QwSTPad # type: ignore
+from qwstpad import DEFAULT_ADDRESS as ADDRESS # type: ignore
+
+pin = Pin("LEDW", Pin.OUT)
+i2c = I2C(id=0, scl=5, sda=4) 
+try:
+    pad = QwSTPad(i2c, ADDRESS)
+except OSError:
+    print("QwSTPad: Not Connected ... Exiting")
+    raise SystemExit
+
+print("QwSTPad: Connected ... Starting")
+
+
+print("LED starts flashing...")
+while True:
+    try:
+        buttons = pad.read_buttons()
+        if(buttons['B']):
+            pin.on()
+        else:
+            pin.off()
+        #print("toggle")
+        #sleep(1) # sleep 1sec
+    except KeyboardInterrupt:
+        break
+pin.off()
+print("Finished.")
